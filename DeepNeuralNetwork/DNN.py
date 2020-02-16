b7e861c88ff6ced474231f2a42056e07316a4d84
NPPprojects <39345956+NPPprojects@users.noreply.github.com>
2020-02-16 16:17:24 +0000

    first commit

---
+++ DeepNeuralNetwork/DNN.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+import keras import Sequential
+from keras.layers import Dense, Activation
+from keras.initializers import Zeros, RandomNormal
+from keras.initializers import glorot_normal, glorot_uniform
---
+++ DeepNeuralNetwork/DisplayMNIST.py
@@ -0,0 +1,18 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from matplotlib import pyplot as plt
+
+(X_train, y_train), (X_valid, y_valid) = mnist.load_data()
+
+X_train.shape
+
+plt.figure(figsize=(10,10))
+for k in range(600):
+    plt.subplot(10, 60, k+1)
+    plt.imshow(X_train[k], cmap='Greys')
+    plt.axis('off')
+plt.tight_layout()
+plt.show()
